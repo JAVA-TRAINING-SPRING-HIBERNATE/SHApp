501c5dd48f1cc3f31eb99679bfeeb4b2f21e898f
bulko <cholewinskimichal@gmail.com>
2015-12-27 21:37:50 +0100

    Added automatially loading of applicationContext

--- workspace/service/src/main/java/pl/cholewinski/controller/SpringRestController.java
+++ workspace/service/src/main/java/pl/cholewinski/controller/SpringRestController.java
@@ -1,13 +1,19 @@
 package pl.cholewinski.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import pl.cholewinski.dao.PersonDAO;
+
 @RestController
 @RequestMapping("/hello")
 public class SpringRestController {
+
+	@Autowired
+	private PersonDAO personDao;
 
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String hello(@PathVariable String name) {
